ec3f38a53a24c6ea5fc258485d05be8f16558276
Julien Lecomte <julien.lecomte@gmail.com>
2022-03-14 21:30:03 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Home_And_Save_Parking_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ASCOM.DriverAccess.Telescope Mount = new ASCOM.DriverAccess.Telescope("ASCOM.iOptron2017.Telescope");
+                Mount.Connected = true;
+                if (Mount.AtPark)
+                {
+                    Mount.Unpark();
+                }
+                Mount.FindHome();
+                Mount.SetPark();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### iOptron CEM70 Find Home And Save Parking Position
+
+This little utility can be used to initialize an iOptron CEM70 telescope mount. It sends custom commands to the mount to make it find its home position, and then saves that position as a custom parking position.
